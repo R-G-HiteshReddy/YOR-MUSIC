fa380e7e6dcda3c4d3460bc50d4cb48695cf4cbc
R-G-HiteshReddy <144146777+R-G-HiteshReddy@users.noreply.github.com>
2023-11-24 19:01:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const songsRoute = require("./routes/songsRoute");
 const adminRoute = require("./routes/adminRoute");
+const cors = require("cors");
 
 app.use("/api/users", userRoute);
 app.use("/api/songs", songsRoute);
 app.use("/api/admin", adminRoute);
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials","true");
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
